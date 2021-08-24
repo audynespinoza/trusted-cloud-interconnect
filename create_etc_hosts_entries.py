157f38cf68853641fb140d6126f1d3ec9293cac0
Audyn Espinoza <audynespinoza@gmail.com>
2021-08-24 13:06:47 -0700

    changed config

--- create_etc_hosts_entries.py
+++ create_etc_hosts_entries.py
@@ -5,13 +5,11 @@ with open('hosts') as f:
 
 host_entries = ''
 
-
 for i in host_info:
     if 'ansible_host=' in i:
         name = i.split()[0].strip()
         ip = i.split('=')[-1].strip()
         host_entries += f'{ip} {name}\n'
-
 
 with open('etc_hosts_entries.txt', 'w') as f:
     f.write(host_entries)
